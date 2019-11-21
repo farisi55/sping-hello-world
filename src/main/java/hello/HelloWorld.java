ee2456ee44afaa22f3ed7ac5faade4c9ba80d7c8
Banu Salman <banus@xl.co.id>
2019-11-21 10:46:16 +0700

    first commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();//untuk manggil class lain
+        System.out.println(greeter.sayHello());//seperti echo
+    }}
